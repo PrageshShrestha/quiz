ae711d0fdcfef2526ad12a5705f7504c5e359432
Abhinav Adhikari <118961560+ABHINAV-cmd-hash@users.noreply.github.com>
2023-09-28 18:33:36 +0545

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,20 @@
+//doesn't work on non windows system
+//should return ascii_value for system
+
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char key_press;
+    int ascii_value;
+    cout<<"\n\t\t\tPress Any Key To Check  Its ASCI Value\n\n\t\t\tPress ESC to EXIT\n\n\n";
+    while(1)
+    {
+    key_press=getch();
+    
+    ascii_value=key_press;
+    cout << ascii_value;
+    return 0;
+}
